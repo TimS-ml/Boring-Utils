31758f9f50a39d6562c643d143e8661fc8b0e42a
TimS <jshen44@fordham.edu>
2024-07-18 22:23:14 +0800

    quick commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='boring-utils',
+    version='0.0.1',
+    description='Boring utils for from scratch implementation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tim S',
+    author_email='jshen44@fordham.edu',
+    url='https://github.com/TimS-ml/Boring-Utils',
+    packages=find_packages()
+)
